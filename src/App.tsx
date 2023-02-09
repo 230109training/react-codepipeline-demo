cca36a2c1f5eddf9fb1e5dff158472803aa925a2
Bach T <bach_tran@outlook.com>
2023-02-09 14:20:17 -0500

    Add an axios request in AppComponent when it first renders

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect }  from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    makeTestRequest();
+  }, []); // empty array as second argument, so when
+  // the component first mounts, the callback function
+  // will run -> implies that it will run only once
+
+  async function makeTestRequest() {
+    let response = await axios.get('http://3.239.80.24:8080/test');
+    console.log(response.data);
+  }
+
   return (
     <h1>Hello from my React App!</h1>
   );
